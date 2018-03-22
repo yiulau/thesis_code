a59c8303a934943829d2417aea4615b1306e65f3
yiulau <yiu.lau@mail.mcgill.ca>
2018-03-22 10:42:43 -0400

    NUTS looks correct. Next focus on increasing speed. Now is super slow.

--- explain_hmc/testnuts_logit.py
+++ explain_hmc/testnuts_logit.py
@@ -1,17 +1,58 @@
+import pandas as pd
 import torch
-from torch.autograd import Variable
+from torch.autograd import Variable,grad
+import pystan
 import numpy
+import pickle
 import time, cProfile, math
-
-dim = 2
-chain_l = 1000
+dim = 5
+num_ob = 100
+chain_l = 500
 burn_in = 100
 max_tdepth = 10
 
+recompile = False
+if recompile:
+    mod = pystan.StanModel(file="./alt_log_reg.stan")
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(mod, f)
 
-def pi(q, p):
-    h = torch.dot(q, q) * 0.5 + torch.dot(p, p) * 0.5
-    return(h)
+mod = pickle.load(open('model.pkl', 'rb'))
+
+
+y_np= numpy.random.binomial(n=1,p=0.5,size=num_ob)
+X_np = numpy.random.randn(num_ob,dim)
+df = pd.read_csv("./pima_india.csv",header=0,sep=" ")
+#print(df)
+dfm = df.as_matrix()
+#print(dfm)
+#print(dfm.shape)
+y_np = dfm[:,8]
+y_np = y_np.astype(numpy.int64)
+X_np = dfm[:,1:8]
+dim = X_np.shape[1]
+num_ob = X_np.shape[0]
+data = dict(y=y_np,X=X_np,N=num_ob,p=dim)
+fit = mod.sampling(data=data,refresh=0)
+
+#print(fit)
+
+y = Variable(torch.from_numpy(y_np).float(),requires_grad=False)
+
+X = Variable(torch.from_numpy(X_np).float(),requires_grad=False)
+
+q = Variable(torch.randn(dim),requires_grad=True)
+p = Variable(torch.randn(dim))
+def pi(q,p):
+    beta = q
+    likelihood = torch.dot(beta, torch.mv(torch.t(X), y)) - \
+                 torch.sum(torch.log(1 + torch.exp(torch.mv(X, beta))))
+    prior = -torch.dot(beta, beta) * 0.5
+    posterior = prior + likelihood
+
+    momentum = torch.dot(p,p) * 0.5
+
+    return(-posterior + momentum)
 
 def logsumexp(a, b):
     s = max(a,b)
@@ -26,7 +67,8 @@ def NUTS(q_init,epsilon,pi,leapfrog,NUTS_criterion):
     p_right = Variable(p.data.clone(),requires_grad=False)
     j = 0
     q_prop = Variable(q_init.data.clone(),requires_grad=True)
-    log_w = -pi(q_init.data,p.data)
+    #log_w = -pi(q_init.data,p.data)
+    log_w = -pi(q_init,p).data.numpy()
     s = True
     while s:
         v = numpy.random.choice([-1,1])
@@ -37,7 +79,7 @@ def NUTS(q_init,epsilon,pi,leapfrog,NUTS_criterion):
             _, _, q_right, p_right, q_prime, s_prime, log_w_prime = BuildTree(q_right, p_right, 1, j, epsilon, leapfrog, pi,
                                                                               NUTS_criterion)
         if s_prime:
-            accep_rate = min(1,math.exp(log_w_prime-log_w))
+            accep_rate = numpy.exp(min(0,(log_w_prime-log_w)))
             u = numpy.random.rand(1)
             if u < accep_rate:
                 q_prop.data = q_prime.data.clone()
@@ -50,7 +92,8 @@ def NUTS(q_init,epsilon,pi,leapfrog,NUTS_criterion):
 def BuildTree(q,p,v,j,epsilon,leapfrog,pi,NUTS_criterion):
     if j ==0:
         q_prime,p_prime = leapfrog(q,p,v*epsilon,pi)
-        log_w_prime = -pi(q_prime.data,p_prime.data)
+        #log_w_prime = -pi(q_prime.data,p_prime.data)
+        log_w_prime = -pi(q_prime, p_prime).data.numpy()
         return q_prime, p_prime, q_prime, p_prime, q_prime, True, log_w_prime
     else:
         # first half of subtree
@@ -62,7 +105,7 @@ def BuildTree(q,p,v,j,epsilon,leapfrog,pi,NUTS_criterion):
             else:
                 _, _, q_right, p_right, q_dprime, s_dprime, log_w_dprime = BuildTree(q_right, p_right, v, j - 1, epsilon,
                                                                                  leapfrog, pi, NUTS_criterion)
-            accep_rate = min(1,math.exp(log_w_dprime-logsumexp(log_w_prime,log_w_dprime)))
+            accep_rate = numpy.exp(min(0,(log_w_dprime-logsumexp(log_w_prime,log_w_dprime))))
             u = numpy.random.rand(1)[0]
             if u < accep_rate:
                 q_prime.data = q_dprime.data.clone()
@@ -93,7 +136,7 @@ def NUTS_criterion(q_left,q_right,p_left,p_right):
         (torch.dot(q_right.data-q_left.data,p_left.data) >=0)
     return(o)
 
-q = Variable(torch.randn(dim),requires_grad=True)
+#q = Variable(torch.randn(dim),requires_grad=True)
 
 v = -1
 
@@ -111,11 +154,12 @@ total = time.time() - begin
 print("total time is {}".format(total))
 print("length of chain is {}".format(chain_l))
 print("length of burn in is {}".format(burn_in))
-print("Use and in criterion")
+print("Use logit")
 store = store[burn_in:,]
 store = store.numpy()
 empCov = numpy.cov(store,rowvar=False)
 emmean = numpy.mean(store,axis=0)
-print(empCov)
-
-print(emmean)
+#print(empCov)
+print("sd is {}".format(numpy.sqrt(numpy.diagonal(empCov))))
+print("mean is {}".format(emmean))
+print(fit)
