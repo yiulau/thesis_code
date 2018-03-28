6bf2820cb6a7d1e874aefedae27c766834eea5cc
Patrick Lau <you@example.com>
2018-03-28 15:59:00 -0400

    xhmc for hmc with unit metric works more or less

--- explain_hmc/testxhmc_logit.py
+++ explain_hmc/testxhmc_logit.py
@@ -7,8 +7,8 @@ import pickle
 import time, cProfile, math
 dim = 5
 num_ob = 100
-chain_l = 20
-burn_in = 10
+chain_l = 1000
+burn_in = 500
 max_tdepth = 10
 
 recompile = False
@@ -33,7 +33,7 @@ X_np = dfm[:,1:8]
 dim = X_np.shape[1]
 num_ob = X_np.shape[0]
 data = dict(y=y_np,X=X_np,N=num_ob,p=dim)
-#fit = mod.sampling(data=data,refresh=0)
+fit = mod.sampling(data=data,refresh=0)
 
 #print(fit)
 
@@ -55,10 +55,48 @@ def pi(q,p):
 
     return(-posterior + momentum)
 
+def pi(q,p):
+    beta = q
+    #print("xbeta {}".format((torch.mv(X, beta))))
+    term1 = torch.sum(logsumexp_torch(Variable(torch.zeros(num_ob)), torch.mv(X, beta)))
+    likelihood = torch.dot(beta, torch.mv(torch.t(X), y)) - \
+                 torch.sum(logsumexp_torch(Variable(torch.zeros(num_ob)), torch.mv(X, beta)))
+    prior = -torch.dot(beta, beta) * 0.5
+    posterior = prior + likelihood
+
+    momentum = torch.dot(p,p) * 0.5
+
+    return(-posterior + momentum)
 def logsumexp(a, b):
     s = max(a,b)
     output = s + math.log((math.exp(a-s) + math.exp(b-s)))
     return(output)
+
+def logsumexp_torch(a,b):
+    # input torch vector
+    s = torch.max(a,b)
+    out = s + torch.log(torch.exp(a-s) + torch.exp(b-s))
+    return(out)
+def stable_sum(a1,logw1,a2,logw2):
+    # output sum_a =  (w1 * a1 + w2 * a2)/(w2 + w1)
+    # log_sum = log(w1 + w2)
+    if (logw2 > logw1):
+        e = math.exp(logw1-logw2)
+        sum_a = (e * a1 + a2)/(1.+e)
+        log_sum = logw2 + math.log(1. + e)
+    else:
+        e = math.exp(logw2 - logw1)
+        sum_a = (e * a2 + a1)/(1.+e)
+        log_sum = logw1 + math.log(1. + e)
+    return(sum_a,log_sum)
+
+def dG_dt(q,p):
+    # q_grad is dU/dq
+    H = pi(q, p)
+    H.backward()
+    q_grad = q.grad.data.clone()
+    q.grad.data.zero_()
+    return(torch.dot(p.data,p.data) - torch.dot(q.data,q_grad))
 
 def NUTS(q_init,epsilon,pi,leapfrog,NUTS_criterion):
     p = Variable(torch.randn(len(q_init)),requires_grad=False)
@@ -70,25 +108,30 @@ def NUTS(q_init,epsilon,pi,leapfrog,NUTS_criterion):
     q_prop = Variable(q_init.data.clone(),requires_grad=True)
     #log_w = -pi(q_init.data,p.data)
     log_w = -pi(q_init,p).data.numpy()
-    sum_p = p.clone()
+    ave = dG_dt(q_init,p)
+    #sum_p = p.clone()
     s = True
     while s:
         v = numpy.random.choice([-1,1])
         if v < 0:
-            q_left, p_left, _, _, q_prime, s_prime, log_w_prime,sum_dp = BuildTree(q_left, p_left, -1, j, epsilon, leapfrog, pi,
+            q_left, p_left, _, _, q_prime, s_prime, log_w_prime,ave_dp = BuildTree(q_left, p_left, -1, j, epsilon, leapfrog, pi,
                                                                             NUTS_criterion)
         else:
-            _, _, q_right, p_right, q_prime, s_prime, log_w_prime, sum_dp = BuildTree(q_right, p_right, 1, j, epsilon, leapfrog, pi,
+            _, _, q_right, p_right, q_prime, s_prime, log_w_prime, ave_dp = BuildTree(q_right, p_right, 1, j, epsilon, leapfrog, pi,
                                                                               NUTS_criterion)
         if s_prime:
             accep_rate = numpy.exp(min(0,(log_w_prime-log_w)))
             u = numpy.random.rand(1)
             if u < accep_rate:
                 q_prop.data = q_prime.data.clone()
-        log_w = logsumexp(log_w,log_w_prime)
-        sum_p = sum_p + sum_dp
+        #log_w = logsumexp(log_w,log_w_prime)
+
+        oo = stable_sum(ave,log_w,ave_dp,log_w_prime)
+        ave = oo[0]
+        log_w_ = oo[1]
         #s = s_prime and NUTS_criterion(q_left,q_right,p_left,p_right)
-        s = s_prime and gen_NUTS_criterion(p_left,p_right,sum_p)
+        #s = s_prime and gen_NUTS_criterion(p_left,p_right,sum_p)
+        s = s_prime and xhmc_criterion(ave,0.1,math.pow(2,j))
         j = j + 1
         s = s and (j<max_tdepth)
     return(q_prop,j)
@@ -98,26 +141,30 @@ def BuildTree(q,p,v,j,epsilon,leapfrog,pi,NUTS_criterion):
         q_prime,p_prime = leapfrog(q,p,v*epsilon,pi)
         #log_w_prime = -pi(q_prime.data,p_prime.data)
         log_w_prime = -pi(q_prime, p_prime).data.numpy()
-        return q_prime, p_prime, q_prime, p_prime, q_prime, True, log_w_prime,p_prime
+        ave = dG_dt(q,p)
+        return q_prime, p_prime, q_prime, p_prime, q_prime, True, log_w_prime, ave
     else:
         # first half of subtree
-        q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime,sum_p = BuildTree(q, p, v, j - 1, epsilon, leapfrog, pi, NUTS_criterion)
+        q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime,ave_prime = BuildTree(q, p, v, j - 1, epsilon, leapfrog, pi, NUTS_criterion)
         # second half of subtree
         if s_prime:
             if v <0:
-                q_left,p_left,_,_,q_dprime,s_dprime,log_w_dprime,sum_dp = BuildTree(q_left,p_left,v,j-1,epsilon,leapfrog,pi,NUTS_criterion)
+                q_left,p_left,_,_,q_dprime,s_dprime,log_w_dprime,ave_dprime = BuildTree(q_left,p_left,v,j-1,epsilon,leapfrog,pi,NUTS_criterion)
             else:
-                _, _, q_right, p_right, q_dprime, s_dprime, log_w_dprime,sum_dp = BuildTree(q_right, p_right, v, j - 1, epsilon,
+                _, _, q_right, p_right, q_dprime, s_dprime, log_w_dprime,ave_dprime = BuildTree(q_right, p_right, v, j - 1, epsilon,
                                                                                  leapfrog, pi, NUTS_criterion)
             accep_rate = numpy.exp(min(0,(log_w_dprime-logsumexp(log_w_prime,log_w_dprime))))
             u = numpy.random.rand(1)[0]
             if u < accep_rate:
                 q_prime.data = q_dprime.data.clone()
             #s_prime = s_dprime and NUTS_criterion(q_left,q_right,p_left,p_right)
-            sum_p = sum_p + sum_dp
-            s_prime = s_dprime and gen_NUTS_criterion(p_left, p_right, sum_p)
-            log_w_prime = logsumexp(log_w_prime,log_w_dprime)
-        return q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime,sum_p
+            oo_ = stable_sum(ave_prime,log_w_prime,ave_dprime,log_w_prime)
+            ave_prime = oo_[0]
+            log_w_prime = oo_[1]
+            #s_prime = s_dprime and gen_NUTS_criterion(p_left, p_right, sum_p)
+            s_prime = s_dprime and xhmc_criterion(ave_prime,0.1,math.pow(2,j))
+            #log_w_prime_ = logsumexp(log_w_prime,log_w_dprime)
+        return q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime, ave_prime
 
 def leapfrog(q,p,epsilon,pi):
     p_prime = Variable(p.data.clone(),requires_grad=False)
@@ -145,13 +192,20 @@ def NUTS_criterion(q_left,q_right,p_left,p_right):
         (torch.dot(q_right.data-q_left.data,p_left.data) >=0)
     return(o)
 
+def xhmc_criterion(ave,xhmc_delta,traj_len):
+    o = abs(ave)/traj_len > xhmc_delta
+    return(o)
 #q = Variable(torch.randn(dim),requires_grad=True)
 
+
+#out = dG_dt(q,p)
+#print(out)
+#exit()
 v = -1
-print(q)
-out = NUTS(q,0.12,pi,leapfrog,NUTS_criterion)
-print(out)
-exit()
+#print(q)
+#out = NUTS(q,0.12,pi,leapfrog,NUTS_criterion)
+#print(out)
+#exit()
 epsilon = 0.11
 
 store = torch.zeros((chain_l,dim))
@@ -175,4 +229,5 @@ print("store is {}".format(store))
 #print(empCov)
 print("sd is {}".format(numpy.sqrt(numpy.diagonal(empCov))))
 print("mean is {}".format(emmean))
-#
+
+print(fit)
