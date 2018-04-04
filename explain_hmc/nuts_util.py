f897ebf4b28747e897f80413c3d26c3d9ac2fe33
Patrick Lau <you@example.com>
2018-04-04 00:04:04 -0400

    utility functions for nuts

--- explain_hmc/nuts_util.py
+++ explain_hmc/nuts_util.py
@@ -31,15 +31,15 @@ def NUTS(q_init,epsilon,H_fun,leapfrog,max_tdepth):
         j = j + 1
         s = s and (j<max_tdepth)
     return(q_prop,j)
-def GNUTS(q_init,epsilon,H_fun,leapfrog,max_tdepth,p_sharp):
+def GNUTS(q_init,epsilon,H_fun,leapfrog,max_tdepth,p_sharp_fun):
     # sum_p should be a tensor instead of variable
     p = Variable(torch.randn(len(q_init)),requires_grad=False)
     q_left = Variable(q_init.data.clone(),requires_grad=True)
     q_right = Variable(q_init.data.clone(),requires_grad=True)
     p_left = Variable(p.data.clone(),requires_grad=False)
     p_right = Variable(p.data.clone(),requires_grad=False)
-    p_sleft = Variable(p_sharp(q_init, p).clone(), requires_grad=False)
-    p_sright = Variable(p_sharp(q_init, p).clone(), requires_grad=False)
+    p_sleft = Variable(p_sharp_fun(q_init, p).clone(), requires_grad=False)
+    p_sright = Variable(p_sharp_fun(q_init, p).clone(), requires_grad=False)
     j = 0
     q_prop = Variable(q_init.data.clone(),requires_grad=True)
     log_w = -H_fun(q_init,p,return_float=True)
@@ -49,10 +49,10 @@ def GNUTS(q_init,epsilon,H_fun,leapfrog,max_tdepth,p_sharp):
         v = numpy.random.choice([-1,1])
         if v < 0:
             q_left, p_left, _, _, q_prime, s_prime, log_w_prime,sum_dp = BuildTree_gnuts(q_left, p_left, -1, j, epsilon, leapfrog, H_fun,
-                                                                            )
+                                                                            p_sharp_fun)
         else:
             _, _, q_right, p_right, q_prime, s_prime, log_w_prime, sum_dp = BuildTree_gnuts(q_right, p_right, 1, j, epsilon, leapfrog, H_fun,
-                                                                              )
+                                                                              p_sharp_fun)
         if s_prime:
             accept_rate = math.exp(min(0,(log_w_prime-log_w)))
             u = numpy.random.rand(1)
@@ -60,8 +60,8 @@ def GNUTS(q_init,epsilon,H_fun,leapfrog,max_tdepth,p_sharp):
                 q_prop.data = q_prime.data.clone()
         log_w = logsumexp(log_w,log_w_prime)
         sum_p += sum_dp
-        p_sleft = p_sharp(q_left, p_left)
-        p_sright = p_sharp(q_right, p_right)
+        p_sleft = p_sharp_fun(q_left, p_left)
+        p_sright = p_sharp_fun(q_right, p_right)
         s = s_prime and gen_NUTS_criterion(p_sleft,p_sright,sum_p)
         j = j + 1
         s = s and (j<max_tdepth)
@@ -89,7 +89,8 @@ def BuildTree_nuts(q,p,v,j,epsilon,leapfrog,H_fun):
             log_w_prime = logsumexp(log_w_prime,log_w_dprime)
         return q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime
 
-def BuildTree_gnuts(q,p,v,j,epsilon,leapfrog,H_fun,p_sharp):
+def BuildTree_gnuts(q,p,v,j,epsilon,leapfrog,H_fun,p_sharp_fun):
+    #p_sharp_fun(q,p) takes tensor returns tensor
     if j ==0:
         q_prime,p_prime = leapfrog(q,p,v*epsilon,H_fun)
         log_w_prime = -H_fun(q_prime, p_prime,return_float=True)
@@ -97,22 +98,26 @@ def BuildTree_gnuts(q,p,v,j,epsilon,leapfrog,H_fun,p_sharp):
     else:
         # first half of subtree
         sum_p = torch.zeros(len(p))
-        q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime,temp_sum_p = BuildTree_gnuts(q, p, v, j - 1, epsilon, leapfrog, H_fun)
+        q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime,temp_sum_p = BuildTree_gnuts(q, p, v, j - 1,
+                                                                                                     epsilon, leapfrog,
+                                                                                                     H_fun,p_sharp_fun)
         sum_p += temp_sum_p
         # second half of subtree
         if s_prime:
             if v <0:
-                q_left,p_left,_,_,q_dprime,s_dprime,log_w_dprime,sum_dp = BuildTree_gnuts(q_left,p_left,v,j-1,epsilon,leapfrog,H_fun)
+                q_left,p_left,_,_,q_dprime,s_dprime,log_w_dprime,sum_dp = BuildTree_gnuts(q_left,p_left,v,j-1,epsilon,
+                                                                                          leapfrog,H_fun,
+                                                                                          p_sharp_fun)
             else:
                 _, _, q_right, p_right, q_dprime, s_dprime, log_w_dprime,sum_dp = BuildTree_gnuts(q_right, p_right, v, j - 1, epsilon,
-                                                                                 leapfrog, H_fun)
+                                                                                 leapfrog, H_fun,p_sharp_fun)
             accept_rate = math.exp(min(0,(log_w_dprime-logsumexp(log_w_prime,log_w_dprime))))
             u = numpy.random.rand(1)[0]
             if u < accept_rate:
                 q_prime.data = q_dprime.data.clone()
             sum_p += sum_dp
-            p_sleft = p_sharp(q_left,p_left)
-            p_sright = p_sharp(q_right,p_right)
+            p_sleft = p_sharp_fun(q_left,p_left)
+            p_sright = p_sharp_fun(q_right,p_right)
             s_prime = s_dprime and gen_NUTS_criterion(p_sleft, p_sright, sum_p)
             log_w_prime = logsumexp(log_w_prime,log_w_dprime)
         return q_left, p_left, q_right, p_right, q_prime, s_prime, log_w_prime,sum_p
@@ -124,10 +129,10 @@ def NUTS_criterion(q_left,q_right,p_left,p_right):
         (torch.dot(q_right.data-q_left.data,p_left.data) >=0)
     return(o)
 
-def gen_NUTS_criterion(p_left,p_right,p_sum):
+def gen_NUTS_criterion(p_sleft,p_sright,p_sum):
     # p_sum should be a tensor
     # True = continue doubling the trajectory
     # False = stop
-    o = (torch.dot(p_left.data,p_sum) >= 0) or \
-        (torch.dot(p_right.data,p_sum) >= 0)
+    o = (torch.dot(p_sleft,p_sum) >= 0) or \
+        (torch.dot(p_sright,p_sum) >= 0)
     return(o)
