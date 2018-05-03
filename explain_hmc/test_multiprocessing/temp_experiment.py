0d9fc0be913b47940417d0cab561c1a5a812008c
yiulau <yiu.lau@mail.mcgill.ca>
2018-05-03 16:03:05 -0400

    download and store data. could be used to clean data soon.

--- explain_hmc/test_multiprocessing/temp_experiment.py
+++ explain_hmc/test_multiprocessing/temp_experiment.py
@@ -1,4 +1,5 @@
 from distributions.logistic_regression import V_logistic_regression
+from distributions.funnel_cp import V_funnel
 from abstract.abstract_class_point import point
 from abstract.abstract_class_Ham import Hamiltonian
 from abstract.metric import metric
@@ -8,14 +9,22 @@ from torch.autograd import Variable
 from abstract.abstract_leapfrog_ult_util import abstract_HMC_alt_ult,abstract_leapfrog_ult,abstract_HMC_alt_windowed,abstract_leapfrog_window
 from abstract.abstract_genleapfrog_ult_util import rmhmc_step,generalized_leapfrog
 from abstract.abstract_nuts_util import abstract_NUTS,abstract_GNUTS,abstract_NUTS_xhmc
+from explicit.genleapfrog_ult_util import softabs_map
 
+#seed=2
+#torch.manual_seed(seed)
+#input = torch.randn(2)*1e-3
+#print(input)
+#output = softabs_map(input,1e-3)
+#print(output)
+#exit()
 
-
-seed=1
-torch.manual_seed(seed)
-numpy.random.seed(seed)
-vo = V_logistic_regression()
-metrico = metric("softabs",vo,alpha=1e3)
+#seed=1
+#torch.manual_seed(seed)
+#numpy.random.seed(seed)
+#vo = V_logistic_regression()
+vo = V_funnel()
+metrico = metric("softabs",vo,alpha=1e6)
 
 #metrico = metric("diag_e",vo)
 from abstract.T_unit_e import T_unit_e
@@ -23,25 +32,25 @@ from abstract.T_unit_e import T_unit_e
 #exit()
 #to = T(metrico,vo)
 Ho = Hamiltonian(vo,metrico)
-epsilon = 0.08
+epsilon = 0.1
 
 qpoint_obj = Ho.V.q_point
 q = qpoint_obj
 #out = abstract_NUTS(q,epsilon,Ho,abstract_leapfrog_ult,5)
-out = abstract_GNUTS(q,epsilon,Ho,generalized_leapfrog,5)
+#out = abstract_GNUTS(q,epsilon,Ho,generalized_leapfrog,5)
 #out = abstract_GNUTS(q,epsilon,Ho,abstract_leapfrog_ult,5)
 #out = abstract_NUTS_xhmc(q,epsilon,Ho,abstract_leapfrog_ult,5,0.1)
 #out = abstract_NUTS_xhmc(q,epsilon,Ho,generalized_leapfrog,5,0.1)
 #out = abstract_HMC_alt_ult(epsilon=0.01,L=10,current_q=qpoint_obj,leapfrog=abstract_leapfrog_ult,Ham=Ho)
-#out = rmhmc_step(qpoint_obj,0.1,20,0.1,Ho)
+out = rmhmc_step(qpoint_obj,0.2,20,Ho)
 #out = abstract_HMC_alt_windowed(epsilon=0.001,L=10,current_q=qpoint_obj,leapfrog_window=abstract_leapfrog_window,Ham=Ho)
 
 q_out = out[0]
-p_out = out[1]
+#p_out = out[1]
 print(q_out.flattened_tensor)
-print(p_out.flattened_tensor)
-print(out[2].flattened_tensor)
-print(out[3:])
+#print(p_out.flattened_tensor)
+#print(out[2].flattened_tensor)
+print(out[1:])
 exit()
 #vo.beta.data = torch.randn(2)
 print(vo.list_var[0].data)
